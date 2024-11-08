0201f41c5cb6ec5dc9c19762876ca5e6ce0173db
gi-greguoldo <160248608+gi-greguoldo@users.noreply.github.com>
2024-11-08 11:46:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Dia de comemorar',
+    'Qual é a data?',
+    '20 de novembro'
+)
+
+criaCartao(
+    'Benefícos',
+    'Qual a importância?',
+    'Evidenciar o problema do racismo e lembrar a resistência dos negros do passado contra a violência praticada contra eles.'
+)
+
+criaCartao(
+    'Quando essa data se tornou importante na sociedade ',
+    'E qual motivo',
+    'Essa data foi instituída no dia 10 de novembro de 2011, e remete ao dia em que foi morto o líder do Quilombo dos Palmares, Zumbi, no ano de 1695.'
+)
+
+criaCartao(
+    'Você sabe o que significa?',
+    'O que é a consciência negra?',
+    'Consciência negra é um termo que ganhou notoriedade na década de 1970, no Brasil, em razão da luta de movimentos sociais que atuavam pela igualdade racial, como o Movimento Negro Unido. O termo é, ao mesmo tempo, uma referência e uma homenagem à cultura ancestral do povo de origem africana, que foi trazido à força e duramente escravizado por séculos no Brasil. É o símbolo da luta, da resistência e a consciência de que a negritude não é inferior e que o negro tem seu valor e seu lugar na sociedade.'
+)
